58ab9f0b76c072af31f2a5b276cb5d2f610fa8ac
litti_10 <m.c.cottonkitten@gmail.com>
2023-04-08 20:20:56 +0900

    add pdf component

--- README.md
+++ README.md
@@ -1,5 +1,48 @@
-# PdfGenerator
+<div align=center>
+	<img src="https://capsule-render.vercel.app/api?type=waving&color=auto&height=200&section=header&text=Marvin%20Github!&fontSize=90" />	
+</div>
 
-![header](https://capsule-render.vercel.app/api?type=slice&color=auto&height=300&section=header&text=NonameD10%20Github!&fontSize=70)
+# [1] Header 
+
+![header](https://capsule-render.vercel.app/api?type=egg&color=auto&height=300&section=header&text=Marvin%20Github!&fontSize=90)
+
+wave (default)
+egg
+shark
+slice
+rect
+soft
+rounded
+cylinder
+waving
+transparent
+
+
+# 📚 Tech Stack 📚
+# ✨ Platforms & Languages ✨
 
 <img src="https://img.shields.io/badge/Python-002323?style=flat&logo=Python&logoColor=red"/>
+
+<img src="https://img.shields.io/badge/C++-004713?style=flat&logo=C&logoColor=yellow"/>
+
+<img src="https://img.shields.io/badge/Python-007396?style=flat&logo=Conda-Forge&logoColor=white"/>
+
+
+### [2] source code
+```python
+import os
+print('hello')
+```
+
+### [3] bullet point
+* bullet point
+    * abc
+        * 123
+
+
+### [4] link 
+[Google](http://usaco.org/)
+
+### [5] image 
+![gold](/resource/gold.png)
+
--- main.py
+++ main.py
@@ -1,87 +1,100 @@
 import tkinter as tk
-from PIL import Image
+from PIL import Image 
 
-from utils import load_main_image, pillow_to_tk_image
-from event import start_event, load_event, save_event
+from event import *
+from utils import *
 
 def main():
-    window = tk.Tk()
-    monitor_width = window.winfo_screenwidth()
+    window = tk.Tk() 
+    monitor_width  = window.winfo_screenwidth()
     monitor_height = window.winfo_screenheight()
+    # window size 
+    window_height = 700
+    window_width  = int(window_height*0.7)
+    # image(logo) size 
+    image_height = window_height - 100
+    image_width  = window_width
+    image_size   = (image_width, image_height) 
 
-    window_height = 1340
-    window_width = int(window_height*0.7)
-
-    image_height = 272
-    image_width = 338
-    image_size = (image_width, image_height)
-
-    # geometry
-    window.geometry('490x700+100+100')
+    # geometry 
+    window.geometry('{}x{}+0+0'.format(window_width, window_height))
     # title
-    window.title('PDF Gen.')
+    window.title('PDF Gen.') 
     window.resizable(False, False)
 
-    # main image load
+    # main image load 
     main_image = load_main_image()
     main_image = main_image.resize(image_size)
-    main_tk_image = pillow_to_tk_image(main_image)
+    main_tk_image = pillow_to_tk_image(main_image) # convert 
 
-    # label
+    # label 
     label = tk.Label(window, image=main_tk_image)
-    label.pack()
-    json_dict={}
-
-    # button_frame 
+    label.pack() 
+    
+    # button frame 
     button_frame = tk.Frame(window)
-    button_frame.pack(side="top", fill='both', expand=True)
-    # [0] start
+    button_frame.pack(side='bottom', fill='both', expand=True)
+    
+    # 1. Start button 
     start_button = tk.Button(
-        button_frame,
-        text='Start',
-        overrelief='solid',
-        width=5,
-        command= lambda: start_event(window, label, json_dict, image_size)
-
+        button_frame, text='Start', overrelief='solid',
+        width=3, command= lambda: start_event(window, label, image_size)
     )
     start_button.pack(side='left', expand=True, fill='x')
-    # [1] load
+
+    
+    # 2. Load button
     load_button = tk.Button(
-        button_frame,
-        text='load',
-        overrelief='solid',
-        width=5,
-        command= lambda: load_event(window, label, json_dict, image_size)
+        button_frame, text='Load Json', overrelief='solid',
+        width=3, command= lambda: load_event(window, label, image_size)
     )
     load_button.pack(side='left', expand=True, fill='x')
-    # [2] save
+    
+    # 3. Save button
     save_button = tk.Button(
-        button_frame,
-        text='Save',
-        overrelief='solid',
-        width=5,
-        command= lambda: save_event(window, label, json_dict)
+        button_frame, text='Save Json', overrelief='solid',
+        width=3, command= lambda: save_event(window)
     )
     save_button.pack(side='left', expand=True, fill='x')
-    # [3] add image
-    addimg_button = tk.Button(
-        button_frame,
-        text='Add image',
-        overrelief='solid',
-        width=5
-    )
-    addimg_button.pack(side='left', expand=True, fill='x')
 
-    # [4] delete image
-    delimg_button = tk.Button(
-        button_frame,
-        text='Remove image',
-        overrelief='solid',
-        width=5
+    # 4. Add Image button 
+    add_image_button = tk.Button(
+        button_frame, text='Add Image', overrelief='solid',
+        width=3, command= lambda: add_image_event(window, label, image_size)
     )
-    delimg_button.pack(side='left', expand=True, fill='x')
+    add_image_button.pack(side='left', expand=True, fill='x')
+
+    # 5. Add Text button 
+    add_text_button = tk.Button(
+        button_frame, text='Add Text', overrelief='solid',
+        width=3, command= lambda: add_text_event(window, label, image_size)
+    )
+    add_text_button.pack(side='left', expand=True, fill='x')
+
+    # 6. Remove button 
+    remove_button = tk.Button(
+        button_frame, text='Remove Item', overrelief='solid',
+        width=3, command= lambda: remove_event(window, label, image_size)
+    )
+    remove_button.pack(side='left', expand=True, fill='x')
+
+    # 7. PDF Generation button 
+    pdf_gen_button = tk.Button(
+        button_frame, text='PDF Gen.', overrelief='solid',
+        width=3, command= lambda: generate_event(image_size)
+    )
+    pdf_gen_button.pack(side='left', expand=True, fill='x')
+
+    # 8. Exit button 
+    exit_button = tk.Button(
+        button_frame, text='Finish', overrelief='solid',
+        width=3, command= lambda: close_event(window)
+    )
+    exit_button.pack(side='left', expand=True, fill='x')
+    
 
     window.mainloop()
 
+
 if __name__ == '__main__':
-    main()
+    main() 
